2a0268635e9927622f16300c297809c4f08e07af
Anirudh S <107941714+AnirudhS1704@users.noreply.github.com>
2024-01-30 18:29:50 +0530

    Update Pgm.java

--- Pgm - 11/Pgm.java
+++ Pgm - 11/Pgm.java
@@ -1,37 +1,58 @@
-import java.io.File; 
-import java.io.FileOutputStream; 
-import java.io.IOException; 
-import java.io.PrintWriter; 
- 
-import javax.servlet.ServletException; 
-import javax.servlet.annotation.WebServlet; 
-import javax.servlet.http.HttpServlet; 
-import javax.servlet.http.HttpServletRequest; 
-import javax.servlet.http.HttpServletResponse; 
- 
-public class ITReturns extends HttpServlet { 
-    private static final long serialVersionUID = 1L; 
-        
-    public ITReturns() { 
-        super(); 
-    } 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws 
-ServletException, IOException { 
-        String name=request.getParameter("name"); 
-        String gender=request.getParameter("gender"); 
-        String salary=request.getParameter("salary"); 
-        String tax=request.getParameter("tax"); 
-        PrintWriter out=response.getWriter(); 
-        File file = new File("/home/mahen/1.txt"); 
-        file.createNewFile(); 
-        FileOutputStream fout = new FileOutputStream(file); 
-        out.println(""+name+gender+salary+tax); 
-        fout.write(("hello"+name+gender+salary+tax).getBytes()); 
-        fout.close(); 
-         
-    } 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws 
-ServletException, IOException { 
-        } 
- 
-} 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * Servlet implementation class ItReturns
+ */
+@WebServlet("ItReturns")
+public class ItReturns extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ItReturns() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("name");
+		String age = request.getParameter("age");
+		String salary = request.getParameter("salary");
+		String tax = request.getParameter("tax");
+		
+		String user = "Name: " + name + "\nAge: " + age + "\nSalary: " + salary + "\nTax: " + tax;
+		
+		PrintWriter out = response.getWriter();
+		out.println(user);
+		
+		File file = new File("/Users/Public/ITRETURN_details.txt");
+		file.createNewFile();
+		
+		FileOutputStream fout = new FileOutputStream(file);
+		fout.write(user.toString().getBytes());
+		fout.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
